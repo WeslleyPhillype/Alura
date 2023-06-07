fe2c2ec848bfce29d7facf187a564ad32678cdb5
Weslley Phillype Tavares Laurentino <weslley.phillype97@gmail.com>
2023-06-07 13:24:27 -0300

    Update programa.java
    teste2

--- Java/programa.java
+++ Java/programa.java
@@ -1,5 +1,7 @@
-public class Programa {
-    public static void main(String[] args) {
+public class Programa{
+
+    public static void main(String[] args){
+
         System.out.println("ola mundo");
     }
 }
