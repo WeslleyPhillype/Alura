feb85f52949dfb50106c91d87c0f5ebe897bae99
Weslley Phillype Tavares Laurentino <weslley.phillype97@gmail.com>
2023-06-11 11:53:39 -0300

    atv-java
    curso java

---
+++ Java/TestaVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] arg) {
+		System.out.println("Ola novo teste");
+		
+		int idade;
+		idade = 25;
+		
+		System.out.println(idade);
+		
+		idade = 25 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) +2;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabéns");
+	}
+
+}
